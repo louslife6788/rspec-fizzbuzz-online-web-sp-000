03fd9e551d0b994d80d39dd0ef57407d30ea0930
luis leon <leonluis380@yahoo.com>
2021-05-13 19:04:39 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,8 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this\
 def fizzbuzz(x)
-  puts "here i am"
+  if x % 3 == 0
+  return "fizz"
 
 
 end
